1f67908a5f97ac3508976e761ba0d62f1e5e24d9
Hend Mahmoud <hend.alqett@gmail.com>
2018-10-16 00:47:33 +0200

    feat: use exprimental Parcelize

--- app/src/main/java/com/hendalqett/wifidetection/data/model/WifiNetwork.kt
+++ app/src/main/java/com/hendalqett/wifidetection/data/model/WifiNetwork.kt
@@ -1,32 +1,11 @@
 package com.hendalqett.wifidetection.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by hend on 10/13/18.
  */
-data class WifiNetwork(var name: String, var level: Int = -300): Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readInt())
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeInt(level)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<WifiNetwork> {
-        override fun createFromParcel(parcel: Parcel): WifiNetwork {
-            return WifiNetwork(parcel)
-        }
-
-        override fun newArray(size: Int): Array<WifiNetwork?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class WifiNetwork(var name: String, var level: Int): Parcelable
