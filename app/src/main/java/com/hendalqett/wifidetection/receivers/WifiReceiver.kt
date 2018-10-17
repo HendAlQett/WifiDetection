18ecd8eae73888112e2f17405ae3ddb5c0288541
Hend Mahmoud <hend.alqett@gmail.com>
2018-10-17 22:59:57 +0200

    refactor: extract broadcast receiver from Activity
    (cherry picked from commit bf389ba)

---
+++ app/src/main/java/com/hendalqett/wifidetection/receivers/WifiReceiver.kt
@@ -0,0 +1,53 @@
+package com.hendalqett.wifidetection.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+
+/**
+ * Created by hend on 10/15/18.
+ */
+//class WifiReceiver : BroadcastReceiver() {
+//
+//    override fun onReceive(context: Context, intent: Intent) {
+//
+//        if (WifiListActivity.wifi?.scanResults?.size!! > 0) {
+//            groupEmptyView.visibility = View.GONE
+//            groupRecyler.visibility = View.VISIBLE
+//        }
+//
+//        presenter.getListOFAllWifi(WifiListActivity.wifi?.scanResults)
+//
+//    }
+//}
+/**
+ * Created by hend on 10/16/18.
+ */
+
+
+class WifiReceiver(context: Context) : BroadcastReceiver() {
+
+    private val context = context.applicationContext
+    private var wifiReceiverListener = context as WifiReceiverListener
+    private var wifiManager: WifiManager
+
+    init {
+        wifiManager = this@WifiReceiver.context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    }
+
+    fun startScan() {
+        context.registerReceiver(this, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+        wifiManager.startScan()
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        wifiReceiverListener.onWifiScannedResults(wifiManager.scanResults)
+    }
+
+    interface WifiReceiverListener {
+        fun onWifiScannedResults(results: List<ScanResult>)
+    }
+}
--- app/src/main/java/com/hendalqett/wifidetection/wifilist/WifiListActivity.kt
+++ app/src/main/java/com/hendalqett/wifidetection/wifilist/WifiListActivity.kt
@@ -1,11 +1,10 @@
 package com.hendalqett.wifidetection.wifilist
 
 import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
@@ -14,6 +13,7 @@ import android.view.View
 import com.hend.airlines.ui.base.BaseActivity
 import com.hendalqett.wifidetection.R
 import com.hendalqett.wifidetection.data.model.WifiNetwork
+import com.hendalqett.wifidetection.receivers.WifiReceiver
 import com.hendalqett.wifidetection.utils.PermissionHandler
 import com.hendalqett.wifidetection.wifidetails.DetailsActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,18 +21,15 @@ import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
 
-class WifiListActivity : BaseActivity<WifiListPresenter>(), WifiListContract.View, WifiAdapter.OnItemClickedListener, WifiAdapter.OnButtonClickedListener {
+class WifiListActivity : BaseActivity<WifiListPresenter>(), WifiListContract.View, WifiAdapter.OnItemClickedListener, WifiAdapter.OnButtonClickedListener, WifiReceiver.WifiReceiverListener {
 
-
-    private var wifiReceiver: WifiReceiver? = null
-
+    private lateinit var wifiReceiver: WifiReceiver
     private lateinit var strongNetworksAdapter: RecyclerView.Adapter<*>
     private lateinit var weakNetworksAdapter: RecyclerView.Adapter<*>
-
     private lateinit var strongNetworksList: MutableList<Any>
     private lateinit var weakNetworksList: MutableList<Any>
 
-    val presenter: WifiListPresenter by inject { parametersOf(this) }
+    private val presenter: WifiListPresenter by inject { parametersOf(this) }
 
     override fun afterInflation(savedInstanceState: Bundle?) {
 
@@ -73,32 +70,16 @@ class WifiListActivity : BaseActivity<WifiListPresenter>(), WifiListContract.Vie
     override fun onResume() {
         super.onResume()
         if (ActivityCompat.checkSelfPermission(this@WifiListActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            wifiReceiver = WifiReceiver()
+            wifiReceiver = WifiReceiver(this@WifiListActivity)
             registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
-            wifi?.startScan()
+            wifiReceiver.startScan()
         }
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (wifiReceiver != null)
-            unregisterReceiver(wifiReceiver)
-    }
-
-    internal inner class WifiReceiver : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-
-            if (wifi?.scanResults?.size!! > 0) {
-                groupEmptyView.visibility = View.GONE
-                groupRecyler.visibility = View.VISIBLE
-            }
-
-            presenter.getListOFAllWifi(wifi?.scanResults)
-
-        }
+        unregisterReceiver(wifiReceiver)
     }
 
     override fun onCloseByUpdate(items: List<WifiNetwork>) {
@@ -131,7 +112,13 @@ class WifiListActivity : BaseActivity<WifiListPresenter>(), WifiListContract.Vie
         presenter.getListOf6TopWeakWifi()
     }
 
-    companion object {
-        private var wifi: WifiManager? = null
+    override fun onWifiScannedResults(results: List<ScanResult>) {
+        if (results.isNotEmpty()) {
+            groupEmptyView.visibility = View.GONE
+            groupRecyler.visibility = View.VISIBLE
+            presenter.getListOFAllWifi(results)
+        }
+
     }
+
 }
